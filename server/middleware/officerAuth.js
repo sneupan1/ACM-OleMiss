2800672ba654465f13b03625a265cd41340eaae5
Saurav Neupane <srvneupane@gmail.com>
2020-09-30 22:01:34 -0500

    added officer authorization middleware, added routes to delete users based on role and authorization

---
+++ server/middleware/officerAuth.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+
+const officerAuth = async (req, res, next) => {
+  try {
+    const token = req.header("x-auth-token");
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findOne({
+      _id: decoded,
+      "tokens.token": token,
+    });
+    if (!user || (user.role !== "admin" && user.role !== "officer")) {
+      throw new Error();
+    }
+    req.token = token;
+    req.user = user;
+    next();
+  } catch (err) {
+    res.status(401).send([{ message: "Not Authorized!" }]);
+  }
+};
+
+module.exports = officerAuth;
--- server/router/profile.js
+++ server/router/profile.js
@@ -1,27 +1,74 @@
 const express = require("express");
 const router = express.Router();
 const Profile = require("../models/profile");
+const User = require("../models/user");
 const auth = require("../middleware/auth");
+const officerAuth = require("../middleware/officerAuth");
 
-//get user profile
+//  @route      POST api/profile/me
+//  @desc       Register basic user profile
+//  @access     Private
 router.get("/me", auth, async (req, res) => {
   try {
     const profile = await Profile.findOne({
       user: req.user._id,
-    }).populate("user", ["name", "email"]);
+    }).populate("user", ["name", "email", "role"]);
     res.send(profile);
   } catch (err) {
     res.status(500).send(err.message);
   }
 });
 
-//get all profiles
+//  @route      POST api/profile/all
+//  @desc       Register basic user profile
+//  @access     Private
 router.get("/all", auth, async (req, res) => {
   try {
-    const profiles = await Profile.find().populate("user", ["name", "email"]);
+    const profiles = await Profile.find().populate("user", [
+      "name",
+      "email",
+      "role",
+    ]);
     res.send(profiles);
   } catch (err) {
     res.status(500).send(err.messsage);
+  }
+});
+
+//  @route      Delete api/profile/me
+//  @desc       Register basic user profile
+//  @access     Private
+router.delete("/me", auth, async (req, res) => {
+  try {
+    await Profile.deleteOne({ user: req.user._id });
+    await req.user.delete();
+    res.send([{ message: "Account Removed Successfully" }]);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+//  @route      Delete api/profile/:id
+//  @desc       Register basic user profile
+//  @access     Private
+router.delete("/:id", officerAuth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      _id: req.params.id,
+    }).populate("user", ["role"]);
+
+    if (req.user.role === "officer" && profile.user.role !== "basic") {
+      return res.status(401).send([
+        {
+          message: "Not authorized to do this!",
+        },
+      ]);
+    }
+    await User.deleteOne({ _id: profile.user._id });
+    await profile.delete();
+    res.send([{ message: "Accont Removed Successfully" }]);
+  } catch (err) {
+    res.status(500).send(err.message);
   }
 });
 
