c0f2f641055dc830e87f42f52fef22108b5eef05
Saurav Neupane <sauravneupane@Sauravs-MacBook-Pro.local>
2023-04-27 10:17:12 -0500

    add health check, remove static serve of client

--- server/index.js
+++ server/index.js
@@ -8,18 +8,12 @@ require("./db/mongoose"); //connect mongoDB
 const PORT = process.env.PORT || 4000;
 app.use(express.json());
 app.use(cors());
+app.get("/", (req, res) => res.send("Server is healthy"));
 app.use("/api/user", require("./router/user"));
 app.use("/api/user", require("./router/officer"));
 app.use("/api/user", require("./router/admin"));
 app.use("/api/profile", require("./router/profile"));
 app.use("/api/event", require("./router/event"));
-
-//set static folder
-app.use(express.static(path.join(__dirname + "/../client/build")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/../client/build/index.html"));
-});
 
 app.listen(PORT, () => {
   console.log("Server is running at port ", PORT);
